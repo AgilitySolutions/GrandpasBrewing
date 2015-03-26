c777978e7d0e5b40ee9da32bbcb75f4b93a9c02e
bivenst <tbivens320@gmail.com>
2015-03-26 14:04:39 -0400

    GB-15 Yeast Flocculation

---
+++ tests/BeerXml/Enums/YeastFlocculationTest.groovy
@@ -0,0 +1,10 @@
+package BeerXml.Enums
+
+class YeastFlocculationTest extends GroovyTestCase {
+    void testGetDescription() {
+        assertEquals("Low", YeastFlocculation.Low.getDescription());
+        assertEquals("Medium", YeastFlocculation.Medium.getDescription());
+        assertEquals("High", YeastFlocculation.High.getDescription());
+        assertEquals("Very High", YeastFlocculation.VeryHigh.getDescription());
+    }
+}
