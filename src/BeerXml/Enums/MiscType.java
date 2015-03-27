d9b846abe8a7fdbcdf0946fd64897a126f3718c1
tim <tbivens320@gmail.com>
2015-03-27 19:45:24 -0400

    GB-11 Misc Type

---
+++ src/BeerXml/Enums/MiscType.java
@@ -0,0 +1,20 @@
+package BeerXml.Enums;
+
+public enum MiscType {
+    Spice("Spice"),
+    Fining("Fining"),
+    WaterAgent("Water Agent"),
+    Herb("Herb"),
+    Flavor("Flavor"),
+    Other("Other");
+
+    MiscType(String description) {
+        _description = description;
+    }
+
+    private String _description;
+
+    public String getDescription() {
+        return _description;
+    }
+}
