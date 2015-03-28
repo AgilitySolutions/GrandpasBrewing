390e903daac30b956553a1de6883c832acb0cc70
tim <tbivens320@gmail.com>
2015-03-27 20:00:33 -0400

    GB-9 Hop Use

---
+++ src/BeerXml/Enums/HopUse.java
@@ -0,0 +1,19 @@
+package BeerXml.Enums;
+
+public enum HopUse {
+    Boil("Boil"),
+    DryHop("Dry Hop"),
+    Mash("Mash"),
+    FirstWort("First Wort"),
+    Aroma("Aroma");
+
+    HopUse(String description) {
+        _description = description;
+    }
+
+    private String _description;
+
+    public String getDescription() {
+        return _description;
+    }
+}
---
+++ tests/BeerXml/Enums/HopUseTest.groovy
@@ -0,0 +1,11 @@
+package BeerXml.Enums
+
+class HopUseTest extends GroovyTestCase {
+    void testGetDescription() {
+        assertEquals("Boil", HopUse.Boil.getDescription());
+        assertEquals("Dry Hop", HopUse.DryHop.getDescription());
+        assertEquals("Mash", HopUse.Mash.getDescription());
+        assertEquals("First Wort", HopUse.FirstWort.getDescription());
+        assertEquals("Aroma", HopUse.Aroma.getDescription());
+    }
+}
