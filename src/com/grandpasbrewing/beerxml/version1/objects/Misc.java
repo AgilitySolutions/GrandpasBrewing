caaa315d4c6b8acd2b6b7af89ec0af9dbb9529b7
bivenst <tbivens320@gmail.com>
2015-04-03 08:24:47 -0400

    GB-24 Misc

---
+++ src/com/grandpasbrewing/beerxml/version1/objects/Misc.java
@@ -0,0 +1,97 @@
+package com.grandpasbrewing.beerxml.version1.objects;
+
+import com.grandpasbrewing.beerxml.version1.enums.MiscType;
+import com.grandpasbrewing.beerxml.version1.enums.MiscUse;
+
+public class Misc extends BeerXmlObjectBase {
+    public MiscType getType() {
+        return _type;
+    }
+
+    public void setType(MiscType type) {
+        _type = type;
+    }
+
+    public MiscUse getUse() {
+        return _use;
+    }
+
+    public void setUse(MiscUse use) {
+        _use = use;
+    }
+
+    public double getTime() {
+        return _time;
+    }
+
+    public void setTime(double time) {
+        _time = time;
+    }
+
+    public double getAmount() {
+        return _amount;
+    }
+
+    public void setAmount(double amount) {
+        _amount = amount;
+    }
+
+    public boolean getAmountIsWeight() {
+        return _amountIsWeight;
+    }
+
+    public void setAmountIsWeight(boolean amountIsWeight) {
+        _amountIsWeight = amountIsWeight;
+    }
+
+    public String getUseFor() {
+        return _useFor;
+    }
+
+    public void setUseFor(String useFor) {
+        _useFor = useFor;
+    }
+
+    public String getNotes() {
+        return _notes;
+    }
+
+    public void setNotes(String notes) {
+        _notes = notes;
+    }
+
+    public String getDisplayAmount() {
+        return _displayAmount;
+    }
+
+    public void setDisplayAmount(String displayAmount) {
+        _displayAmount = displayAmount;
+    }
+
+    public String getInventory() {
+        return _inventory;
+    }
+
+    public void setInventory(String inventory) {
+        _inventory = inventory;
+    }
+
+    public String getDisplayTime() {
+        return _displayTime;
+    }
+
+    public void setDisplayTime(String displayTime) {
+        _displayTime = displayTime;
+    }
+
+    private MiscType _type;
+    private MiscUse _use;
+    private double _time;
+    private double _amount;
+    private boolean _amountIsWeight;
+    private String _useFor;
+    private String _notes;
+    private String _displayAmount;
+    private String _inventory;
+    private String _displayTime;
+}
