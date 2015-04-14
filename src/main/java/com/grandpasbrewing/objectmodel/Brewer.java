9d2896100276ccd3d1493656c579d461c0ab8212
bivenst <tbivens320@gmail.com>
2015-04-14 07:51:47 -0400

    GB-59 Brewer object

---
+++ src/main/java/com/grandpasbrewing/objectmodel/Brewer.java
@@ -0,0 +1,13 @@
+package com.grandpasbrewing.objectmodel;
+
+public class Brewer extends ObjectModelBase {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+}
---
+++ src/main/java/com/grandpasbrewing/objectmodel/ObjectModelBase.java
@@ -0,0 +1,13 @@
+package com.grandpasbrewing.objectmodel;
+
+public abstract class ObjectModelBase {
+    private int _id;
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int id) {
+        _id = id;
+    }
+}
