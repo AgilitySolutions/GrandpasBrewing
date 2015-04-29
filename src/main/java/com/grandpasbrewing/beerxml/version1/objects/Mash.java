d6746f71932701c682ef75b7722f3c221779a423
bivenst <tbivens320@gmail.com>
2015-04-29 08:13:05 -0400

    GB-36 Mash Deserializer

--- src/main/java/com/grandpasbrewing/beerxml/version1/objects/Mash.java
+++ src/main/java/com/grandpasbrewing/beerxml/version1/objects/Mash.java
@@ -1,13 +1,18 @@
 package com.grandpasbrewing.beerxml.version1.objects;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
+@XmlRootElement(name = "MASH")
 public class Mash extends BeerXmlObjectBase {
-    public double getGrainTemperature() {
+    public Double getGrainTemperature() {
         return _grainTemperature;
     }
 
-    public void setGrainTemperature(double grainTemperature) {
+    @XmlElement(name = "GRAIN_TEMP")
+    public void setGrainTemperature(Double grainTemperature) {
         _grainTemperature = grainTemperature;
     }
 
@@ -15,6 +20,8 @@ public class Mash extends BeerXmlObjectBase {
         return _mashSteps;
     }
 
+    @XmlElementWrapper(name = "MASH_STEPS")
+    @XmlElement(name = "MASH_STEP")
     public void setMashSteps(ArrayList<MashStep> mashSteps) {
         _mashSteps = mashSteps;
     }
@@ -23,55 +30,62 @@ public class Mash extends BeerXmlObjectBase {
         return _notes;
     }
 
+    @XmlElement(name = "NOTES")
     public void setNotes(String notes) {
         _notes = notes;
     }
 
-    public double getTunTemperature() {
+    public Double getTunTemperature() {
         return _tunTemperature;
     }
 
-    public void setTunTemperature(double tunTemperature) {
+    @XmlElement(name = "TUN_TEMP")
+    public void setTunTemperature(Double tunTemperature) {
         _tunTemperature = tunTemperature;
     }
 
-    public double getSpargeTemperature() {
+    public Double getSpargeTemperature() {
         return _spargeTemperature;
     }
 
-    public void setSpargeTemperature(double spargeTemperature) {
+    @XmlElement(name = "SPARGE_TEMP")
+    public void setSpargeTemperature(Double spargeTemperature) {
         _spargeTemperature = spargeTemperature;
     }
 
-    public double getPH() {
+    public Double getPH() {
         return _ph;
     }
 
-    public void setPH(double ph) {
+    @XmlElement(name = "PH")
+    public void setPH(Double ph) {
         _ph = ph;
     }
 
-    public double getTunWeight() {
+    public Double getTunWeight() {
         return _tunWeight;
     }
 
-    public void setTunWeight(double tunWeight) {
+    @XmlElement(name = "TUN_WEIGHT")
+    public void setTunWeight(Double tunWeight) {
         _tunWeight = tunWeight;
     }
 
-    public double getTunSpecificHeat() {
+    public Double getTunSpecificHeat() {
         return _tunSpecificHeat;
     }
 
-    public void setTunSpecificHeat(double tunSpecificHeat) {
+    @XmlElement(name = "TUN_SPECIFIC_HEAT")
+    public void setTunSpecificHeat(Double tunSpecificHeat) {
         _tunSpecificHeat = tunSpecificHeat;
     }
 
-    public boolean getEquipmentAdjust() {
+    public Boolean getEquipmentAdjust() {
         return _equipmentAdjust;
     }
 
-    public void setEquipmentAdjust(boolean equipmentAdjust) {
+    @XmlElement(name = "EQUIP_ADJUST")
+    public void setEquipmentAdjust(Boolean equipmentAdjust) {
         _equipmentAdjust = equipmentAdjust;
     }
 
@@ -79,6 +93,7 @@ public class Mash extends BeerXmlObjectBase {
         return _displayGrainTemperature;
     }
 
+    @XmlElement(name = "DISPLAY_GRAIN_TEMP")
     public void setDisplayGrainTemperature(String displayGrainTemperature) {
         _displayGrainTemperature = displayGrainTemperature;
     }
@@ -87,6 +102,7 @@ public class Mash extends BeerXmlObjectBase {
         return _displayTunTemperature;
     }
 
+    @XmlElement(name = "DISPLAY_TUN_TEMP")
     public void setDisplayTunTemperature(String displayTunTemperature) {
         _displayTunTemperature = displayTunTemperature;
     }
@@ -95,6 +111,7 @@ public class Mash extends BeerXmlObjectBase {
         return _displaySpargeTemperature;
     }
 
+    @XmlElement(name = "DISPLAY_SPARGE_TEMP")
     public void setDisplaySpargeTemperature(String displaySpargeTemperature) {
         _displaySpargeTemperature = displaySpargeTemperature;
     }
@@ -103,19 +120,20 @@ public class Mash extends BeerXmlObjectBase {
         return _displayTunWeight;
     }
 
+    @XmlElement(name = "DISPLAY_TUN_WEIGHT")
     public void setDisplayTunWeight(String displayTunWeight) {
         _displayTunWeight = displayTunWeight;
     }
 
-    private double _grainTemperature;
+    private Double _grainTemperature;
     private ArrayList<MashStep> _mashSteps;
     private String _notes;
-    private double _tunTemperature;
-    private double _spargeTemperature;
-    private double _ph;
-    private double _tunWeight;
-    private double _tunSpecificHeat;
-    private boolean _equipmentAdjust;
+    private Double _tunTemperature;
+    private Double _spargeTemperature;
+    private Double _ph;
+    private Double _tunWeight;
+    private Double _tunSpecificHeat;
+    private Boolean _equipmentAdjust;
     private String _displayGrainTemperature;
     private String _displayTunTemperature;
     private String _displaySpargeTemperature;
