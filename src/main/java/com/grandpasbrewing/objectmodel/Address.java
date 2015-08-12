2a32a413879312aba63521b9d06d2d6cf043133a
bivenst <tbivens320@gmail.com>
2015-08-12 08:11:49 -0400

    GB-113 Supplier

---
+++ src/main/java/com/grandpasbrewing/objectmodel/Address.java
@@ -0,0 +1,13 @@
+package com.grandpasbrewing.objectmodel;
+
+import com.grandpasbrewing.objectmodel.enums.State;
+
+public class Address extends ObjectModelBase {
+    private String _line1;
+    private String _line2;
+    private String _city;
+    private State _state;
+    private String _zipcode;
+    private double _latitude;
+    private double _longitude;
+}
---
+++ src/main/java/com/grandpasbrewing/objectmodel/Supplier.java
@@ -0,0 +1,13 @@
+package com.grandpasbrewing.objectmodel;
+
+public class Supplier extends ObjectModelBase {
+    private String Name;
+    private String Url;
+    private String Username;
+    private Address MailingAddress;
+    private String Email;
+    private String Phone;
+    private String Fax;
+    private String Contact;
+    private String CustomerNumber;
+}
