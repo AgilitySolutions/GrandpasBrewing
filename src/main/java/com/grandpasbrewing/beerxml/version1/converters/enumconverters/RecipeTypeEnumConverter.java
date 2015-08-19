611b81b7a11d8b51cfdd932d3189c6c7056e0a83
bivenst <tbivens320@gmail.com>
2015-08-19 15:08:24 -0400

    GB-217 RecipeTypeEnumConverter

---
+++ src/main/java/com/grandpasbrewing/beerxml/version1/converters/enumconverters/RecipeTypeEnumConverter.java
@@ -0,0 +1,20 @@
+package com.grandpasbrewing.beerxml.version1.converters.enumconverters;
+
+import com.grandpasbrewing.beerxml.version1.enums.RecipeType;
+import com.grandpasbrewing.beerxml.version1.interfaces.converters.enumconverters.IEnumConverter;
+
+public class RecipeTypeEnumConverter implements IEnumConverter<RecipeType, com.grandpasbrewing.objectmodel.enums.RecipeType> {
+    public com.grandpasbrewing.objectmodel.enums.RecipeType toObjectModel(RecipeType enumValue) {
+        switch (enumValue)
+        {
+            case Extract:
+                return com.grandpasbrewing.objectmodel.enums.RecipeType.Extract;
+            case PartialMash:
+                return com.grandpasbrewing.objectmodel.enums.RecipeType.PartialMash;
+            case AllGrain:
+                return com.grandpasbrewing.objectmodel.enums.RecipeType.AllGrain;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}
