ce98473aac68a4687e14caa9e494918292bd9853
bivenst <tbivens320@gmail.com>
2015-09-08 07:39:22 -0400

    GB-255 script MiscStepTypes

---
+++ src/main/java/com/grandpasbrewing/data/migrations/scripts/data/V1__populate_mash_step_types.sql
@@ -0,0 +1,3 @@
+    insert into MashStepTypes (ID, Name, Description) values (1, 'Infusion', 'Infusion')
+    insert into MashStepTypes (ID, Name, Description) values (2, 'Temperature', 'Temperature')
+    insert into MashStepTypes (ID, Name, Description) values (3, 'Decoction', 'Decoction')
