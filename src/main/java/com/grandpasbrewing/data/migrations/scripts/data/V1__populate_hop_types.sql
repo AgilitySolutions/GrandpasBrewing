40f2c8e36ee95956d5e136fc6293def89aca5c81
bivenst <tbivens320@gmail.com>
2015-09-08 07:36:27 -0400

    GB-254 script Hop Uses

---
+++ src/main/java/com/grandpasbrewing/data/migrations/scripts/data/V1__populate_hop_types.sql
@@ -0,0 +1,3 @@
+  insert into HopTypes (ID, Name, Description) values (1, 'Bittering', 'Bittering')
+  insert into HopTypes (ID, Name, Description) values (2, 'Aroma', 'Aroma')
+  insert into HopTypes (ID, Name, Description) values (3, 'Both', 'Both')
