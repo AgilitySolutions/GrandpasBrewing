d89738007f0111743103ddb0da4366a0386260c6
bivenst <tbivens320@gmail.com>
2015-09-08 07:59:20 -0400

    GB-263 script Yeast Flocculations

---
+++ src/main/java/com/grandpasbrewing/data/migrations/scripts/data/V1__populate_yeast_flocculations.sql
@@ -0,0 +1,4 @@
+    insert into YeastFlocculations (ID, Name, Description) values (1, 'Low', 'Low')
+    insert into YeastFlocculations (ID, Name, Description) values (2, 'Medium', 'Medium')
+    insert into YeastFlocculations (ID, Name, Description) values (3, 'High', 'High')
+    insert into YeastFlocculations (ID, Name, Description) values (4, 'VeryHigh', 'Very High')
