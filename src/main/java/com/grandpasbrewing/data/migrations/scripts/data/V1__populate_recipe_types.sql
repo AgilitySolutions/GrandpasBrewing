bf8020530561566e1f155efd470e45fc33835062
bivenst <tbivens320@gmail.com>
2015-09-08 07:52:33 -0400

    GB-259 script Recipe Types

---
+++ src/main/java/com/grandpasbrewing/data/migrations/scripts/data/V1__populate_recipe_types.sql
@@ -0,0 +1,3 @@
+    insert into RecipeTypes (ID, Name, Description) values (1, 'Extract', 'Extract')
+    insert into RecipeTypes (ID, Name, Description) values (2, 'PartialMash', 'Partial Mash')
+    insert into RecipeTypes (ID, Name, Description) values (3, 'AllGrain', 'All Grain')
