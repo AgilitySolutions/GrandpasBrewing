576c374c6474478dea2f6841dcd142a7df61ecdd
bivenst <tbivens320@gmail.com>
2016-03-18 13:09:10 -0400

    GB-180 Flyway

---
+++ tests/groovy/com/grandpasbrewing/beerxml/version1/converters/JUnitTestForTFSBuild.java
@@ -0,0 +1,29 @@
+package com.grandpasbrewing.beerxml.version1.converters;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bivenst on 3/18/2016.
+ */
+public class JUnitTestForTFSBuild {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSomething() throws Exception {
+        Assert.assertFalse(false);
+    }
+}
