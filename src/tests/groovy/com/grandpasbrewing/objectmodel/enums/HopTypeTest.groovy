68a68e3e16df1ffd4e03cd1bdb30bc53b313ed64
bivenst <tbivens320@gmail.com>
2016-04-25 08:42:31 -0400

    GB-180 Flyway

--- src/tests/groovy/com/grandpasbrewing/objectmodel/enums/HopTypeTest.groovy
+++
@@ -1,9 +0,0 @@
-package com.grandpasbrewing.objectmodel.enums
-
-class HopTypeTest extends GroovyTestCase {
-    void testGetDescription() {
-        assertEquals("Bittering", HopType.Bittering.getDescription());
-        assertEquals("Aroma", HopType.Aroma.getDescription());
-        assertEquals("Both", HopType.Both.getDescription());
-    }
-}
--- src/tests/groovy/com/grandpasbrewing/objectmodel/enums/MashStepTypeTest.groovy
+++
@@ -1,9 +0,0 @@
-package com.grandpasbrewing.objectmodel.enums
-
-class MashStepTypeTest extends GroovyTestCase {
-    void testGetDescription() {
-        assertEquals("Infusion", MashStepType.Infusion.getDescription());
-        assertEquals("Temperature", MashStepType.Temperature.getDescription());
-        assertEquals("Decoction", MashStepType.Decoction.getDescription());
-    }
-}
